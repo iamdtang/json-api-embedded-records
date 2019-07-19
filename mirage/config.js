388c59a868e16ea70348ce6cc874d5da150376fb
David Tang <david.tang@verizondigitalmedia.com>
2019-07-19 16:40:27 -0700

    jsonapiadapter with jsonserializer

---
+++ mirage/config.js
@@ -0,0 +1,46 @@
+import { Response } from 'ember-cli-mirage';
+
+export default function() {
+  this.get('/posts', () => {
+    return new Response(200, {}, {
+      "data": [
+        {
+          "id": "1",
+          "type": "post",
+          "attributes": {
+            "title": "Post A",
+            "body": "...",
+            "tags": [
+              {
+                "id": "1",
+                "name": "JavaScript"
+              },
+              {
+                "id": "2",
+                "name": "Node.js"
+              }
+            ]
+          }
+        },
+        {
+          "id": "2",
+          "type": "post",
+          "attributes": {
+            "title": "Post B",
+            "body": "...",
+            "tags": [
+              {
+                "id": "1",
+                "name": "JavaScript"
+              },
+              {
+                "id": "3",
+                "name": "Ember.js"
+              }
+            ]
+          }
+        }
+      ]
+    });
+  });
+}
