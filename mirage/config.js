53843c9e621e7907a180ea6e5c6306e2fb5ca618
David Tang <david.tang@verizondigitalmedia.com>
2019-07-21 09:21:01 -0700

    reformat /posts endpoint

--- mirage/config.js
+++ mirage/config.js
@@ -3,39 +3,39 @@ import { Response } from 'ember-cli-mirage';
 export default function() {
   this.get('/posts', () => {
     return new Response(200, {}, {
-      "data": [
+      data: [
         {
-          "id": "1",
-          "type": "post",
-          "attributes": {
-            "title": "Post A",
-            "body": "...",
-            "tags": [
+          id: '1',
+          type: 'post',
+          attributes: {
+            title: 'Post A',
+            body: '...',
+            tags: [
               {
-                "id": "1",
-                "name": "JavaScript"
+                id: '1',
+                'name': 'JavaScript'
               },
               {
-                "id": "2",
-                "name": "Node.js"
+                id: '2',
+                name: 'Node.js'
               }
             ]
           }
         },
         {
-          "id": "2",
-          "type": "post",
-          "attributes": {
-            "title": "Post B",
-            "body": "...",
-            "tags": [
+          id: '2',
+          type: 'post',
+          attributes: {
+            title: 'Post B',
+            body: '...',
+            tags: [
               {
-                "id": "1",
-                "name": "JavaScript"
+                id: '1',
+                'name': 'JavaScript'
               },
               {
-                "id": "3",
-                "name": "Ember.js"
+                id: '3',
+                name: 'Ember.js'
               }
             ]
           }
